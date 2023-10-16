cbb31f526ff90a7f416d63da49ff363d381ffbce
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2023-10-16 08:09:40 -0400

    Avoid collisions with sign placement

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ let playerOneTurn = true;
 
 boxes.forEach((button) => {
     button.addEventListener("click", (evt) => {
-        if (playerOneTurn == true) {
-            evt.target.textContent = "X";
-            playerOneTurn = false;
-        } else {
-            evt.target.textContent = "O";
-            playerOneTurn = true;
+        if (evt.target.textContent == "") {
+            if (playerOneTurn == true) {
+                evt.target.textContent = "X";
+                playerOneTurn = false;
+            } else {
+                evt.target.textContent = "O";
+                playerOneTurn = true;
+            }
         }
     });
 });
