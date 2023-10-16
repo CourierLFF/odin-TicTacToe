26469182f4a30c41aa058b3ba9f9913fc4565612
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2023-10-16 10:43:24 -0400

    Add win conditions

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ let playerOneTurn = true;
 
 const playerOneGrid = [];
 const playerTwoGrid = [];
+const winConditions = [
+    ["0", "1", "2"],
+    ["3", "4", "5"],
+    ["6", "7", "8"],
+    ["0", "3", "6"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["0", "4", "8"],
+    ["2", "4", "6"]
+];
 
 const boxes = document.querySelectorAll(".box");
 const resetButton = document.querySelector("#reset-button");
@@ -13,10 +23,12 @@ boxes.forEach((box) => {
                 evt.target.textContent = "X";
                 playerOneGrid.push(evt.target.dataset.position);
                 playerOneTurn = false;
+                checkWin();
             } else {
                 evt.target.textContent = "O";
                 playerTwoGrid.push(evt.target.dataset.position);
                 playerOneTurn = true;
+                checkWin();
             }
         }
     });
@@ -29,3 +41,13 @@ resetButton.addEventListener("click", () => {
     playerOneGrid.splice(0, playerOneGrid.length);
     playerTwoGrid.splice(0, playerTwoGrid.length);
 });
+
+function checkWin() {
+    for(i = 0; i < winConditions.length; i++) {
+        if (winConditions[i].every(elem => playerOneGrid.includes(elem))) {
+            console.log("Player 1 Wins!");
+        } else if (winConditions[i].every(elem => playerTwoGrid.includes(elem))) {
+            console.log("Player 2 Wins!");
+        }
+    }
+}
