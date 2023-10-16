1c55c5a26a6267455cc1c36cb9cab292b4cba9a2
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2023-10-16 08:21:37 -0400

    Add sign positions to array

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const boxes = document.querySelectorAll(".box");
-
 let playerOneTurn = true;
 
-boxes.forEach((button) => {
-    button.addEventListener("click", (evt) => {
+const playerOneGrid = [];
+const playerTwoGrid = [];
+
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+    box.addEventListener("click", (evt) => {
         if (evt.target.textContent == "") {
             if (playerOneTurn == true) {
                 evt.target.textContent = "X";
+                playerOneGrid.push(evt.target.dataset.position);
                 playerOneTurn = false;
             } else {
                 evt.target.textContent = "O";
+                playerTwoGrid.push(evt.target.dataset.position);
                 playerOneTurn = true;
             }
         }
