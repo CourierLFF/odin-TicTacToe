f1502808d3c8e8908e943e53ac526cef3019061e
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2023-10-17 22:14:21 -0400

    Add info on turns and wins to html

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const boxes = document.querySelectorAll(".box");
 const resetButton = document.querySelector("#reset-button");
 const playerOneScoreElem = document.querySelector("#player-one-score");
 const playerTwoScoreElem = document.querySelector("#player-two-score");
+const turnInfo = document.querySelector("#turn-info")
 
 boxes.forEach((box) => {
     box.addEventListener("click", (evt) => {
@@ -29,11 +30,13 @@ boxes.forEach((box) => {
                     evt.target.textContent = "X";
                     playerOneGrid.push(evt.target.dataset.position);
                     playerOneTurn = false;
+                    turnInfo.textContent = "Player Two Turn!";
                     checkWin();
                 } else {
                     evt.target.textContent = "O";
                     playerTwoGrid.push(evt.target.dataset.position);
                     playerOneTurn = true;
+                    turnInfo.textContent = "Player One Turn!";
                     checkWin();
                 }
             }
@@ -47,6 +50,8 @@ resetButton.addEventListener("click", () => {
     })
     playerOneGrid.splice(0, playerOneGrid.length);
     playerTwoGrid.splice(0, playerTwoGrid.length);
+    turnInfo.textContent = "Player One Turn!";
+    playerOneTurn = true;
     gameInProgress = true;
 });
 
@@ -54,14 +59,16 @@ function checkWin() {
     for(i = 0; i < winConditions.length; i++) {
         if (winConditions[i].every(elem => playerOneGrid.includes(elem))) {
             playerOneScore += 1;
-            playerOneScoreElem.textContent = `Player 1 Score: ${playerOneScore}`;
+            playerOneScoreElem.textContent = `Player One Score: ${playerOneScore}`;
+            turnInfo.textContent = "Player One Wins!";
             gameInProgress = false;
         } else if (winConditions[i].every(elem => playerTwoGrid.includes(elem))) {
             playerTwoScore += 1;
-            playerTwoScoreElem.textContent = `Player 2 Score: ${playerTwoScore}`;
+            playerTwoScoreElem.textContent = `Player Two Score: ${playerTwoScore}`;
+            turnInfo.textContent = "Player Two Wins!";
             gameInProgress = false;
         } else if (playerOneGrid.length + playerTwoGrid.length == 9) {
-            console.log("It's a draw!")
+            turnInfo.textContent = "It's a Draw!";
             gameInProgress = false;
             break;
         }
